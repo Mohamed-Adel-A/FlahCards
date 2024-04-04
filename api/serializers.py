109777f36867b4e40a9703a897a79b59cf42b858
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-04-05 01:18:05 +0200

    adding test.rest

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from flashCards.models import Collection, Card
+from django.contrib.auth.models import User
+
 
 class CollectionSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,3 +12,14 @@ class CardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Card
         fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'email']
--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import CollectionViewSet, CardViewSet
+from .views import CollectionViewSet, CardViewSet, UserRegisterAPIView, UserLoginAPIView # UserViewSet
 
 router = routers.DefaultRouter()
 router.register(r'collections', CollectionViewSet)
 router.register(r'cards', CardViewSet)
 
+# user_router = routers.DefaultRouter()
+# user_router.register(r'users', UserViewSet, basename='users')
+
 urlpatterns = [
     path('', include(router.urls)),
+    path('users/', include('knox.urls')),
+    path('signup/', UserRegisterAPIView.as_view()),
+    path('login/', UserLoginAPIView.as_view()),
+    
 ]
