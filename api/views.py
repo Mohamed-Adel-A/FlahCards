95b4718acd4a810e6a50fba67b96ca70143e2d55
MDev <email@example.com>
2024-04-04 16:20:57 +0200

    Add "knox" for api authentication (https://github.com/jazzband/django-rest-knox)

--- api/views.py
+++ api/views.py
@@ -1,11 +1,13 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from flashCards.models import Collection, Card
 from .serializers import CollectionSerializer, CardSerializer
 
 class CollectionViewSet(viewsets.ModelViewSet):
     queryset = Collection.objects.all()
     serializer_class = CollectionSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 class CardViewSet(viewsets.ModelViewSet):
     queryset = Card.objects.all()
     serializer_class = CardSerializer
+    permission_classes = [permissions.IsAuthenticated]
--- backend/settings.py
+++ backend/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'flashCards',
     'rest_framework',
+    'knox',
     'api',
 ]
 
@@ -130,3 +131,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication',),
+}
