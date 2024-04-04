537ea5cc070b6bc324e0535503f7c7714635496c
MDev <email@example.com>
2024-04-04 15:41:21 +0200

    Add api views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from flashCards.models import Collection, Card
+from .serializers import CollectionSerializer, CardSerializer
 
-# Create your views here.
+class CollectionViewSet(viewsets.ModelViewSet):
+    queryset = Collection.objects.all()
+    serializer_class = CollectionSerializer
+
+class CardViewSet(viewsets.ModelViewSet):
+    queryset = Card.objects.all()
+    serializer_class = CardSerializer
