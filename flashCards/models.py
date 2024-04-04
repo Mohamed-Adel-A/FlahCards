c95933ad94e81e0cea1481e2c32c72a58e1990a3
MDev <email@example.com>
2024-04-04 15:03:56 +0200

    Add Collection and Card models to the app

--- flashCards/models.py
+++ flashCards/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Collection(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    category = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Card(models.Model):
+    front = models.TextField()
+    back = models.TextField()
+    collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.front
