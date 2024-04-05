8df46f5f480354d40b21357b0c0a6beb9ea6388e
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-04-05 15:11:50 +0200

    Add collection and card views

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import CollectionViewSet, CardViewSet, UserRegisterAPIView, UserLoginAPIView # UserViewSet
+from .views import UserRegisterAPIView, UserLoginAPIView, UserLogoutAPIView #CardViewSet, CollectionViewSet,  UserViewSet
+from .views import CollectionCreate, CollectionRetrieveUpdateDestroy
+from .views import CardCreate, CardRetrieveUpdateDestroy
+
 
 router = routers.DefaultRouter()
-router.register(r'collections', CollectionViewSet)
-router.register(r'cards', CardViewSet)
+"""
+router.register(r'collections', CollectionViewSet, basename='collections')
+router.register(r'cards', CardViewSet, basename='cards')
+"""
 
 # user_router = routers.DefaultRouter()
 # user_router.register(r'users', UserViewSet, basename='users')
@@ -14,5 +19,9 @@ urlpatterns = [
     path('users/', include('knox.urls')),
     path('signup/', UserRegisterAPIView.as_view()),
     path('login/', UserLoginAPIView.as_view()),
-    
+    path('logout/', UserLogoutAPIView.as_view()),
+    path('collections/', CollectionCreate.as_view()),
+    path('collections/<int:pk>/', CollectionRetrieveUpdateDestroy.as_view()),
+    path('collections/<int:collection>/cards/', CardCreate.as_view()),
+    path('collections/<int:collection>/cards/<int:pk>/', CardRetrieveUpdateDestroy.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,23 +1,83 @@
-from rest_framework import viewsets, permissions, status
+from rest_framework import viewsets, permissions, status, generics
 from flashCards.models import Collection, Card
 from .serializers import CollectionSerializer, CardSerializer, UserSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from knox.models import AuthToken
 from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import get_object_or_404
 
 from rest_framework.views import APIView
 
-class CollectionViewSet(viewsets.ModelViewSet):
-    queryset = Collection.objects.all()
+
+class CollectionCreate(generics.ListCreateAPIView):
     serializer_class = CollectionSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Collection.objects.filter(user=user).order_by('-created_at')
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class CollectionRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = CollectionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Collection.objects.filter(user=user)
+
+
+## Card views ##
+class CardCreate(generics.ListCreateAPIView):
+
+    serializer_class = CardSerializer
+    permission_classes=[permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        #collection = self.kwargs.get("collection")
+        collection = get_object_or_404(Collection, pk=self.kwargs["collection"])
+        print(collection)
+        return Card.objects.filter(collection=collection).order_by('-created_at')
+    
+    def perform_create(self, serializer):
+        serializer.save(collection=self.request.collection)
+
+
+class CardRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = CardSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        collection = get_object_or_404(Collection, pk=self.kwargs["collection"])
+        #collection = self.kwargs['collection']
+        return Card.objects.filter(collection=collection)
+
+
+############################
+
+
+
+"""
+class CollectionViewSet(viewsets.ModelViewSet):
+#    queryset = Collection.objects.all()
+    serializer_class = CollectionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Collection.objects.filter(user=user).order_by('-created_at')
+
 
 class CardViewSet(viewsets.ModelViewSet):
     queryset = Card.objects.all()
     serializer_class = CardSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+"""
 
 class UserRegisterAPIView(APIView):
     permission_classes = [permissions.AllowAny]
