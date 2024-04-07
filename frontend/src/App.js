82e6accb399e0ef6f5b8677e6e45f0b74291a4a9
MDev <email@example.com>
2024-04-07 12:34:28 +0200

    Frontend: Add Naviagation bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
@@ -11,9 +12,38 @@ import CollectionAddEdit from './components/collection-add-edit';
 import CardsList from './components/cards-list';
 import CardAddEdit from './components/card-add-edit';
 
+import { Link } from 'react-router-dom';
+
 function App() {
+  const [user, setUser] = useState(null);
+
   return (
     <div className="App">
+      <Navbar className="bg-body-tertiary" bg="dark" variant="dark" data-bs-theme="dark">
+      <div className="container-fluid">
+      <Container>
+        <Navbar.Brand href="#home">FlasCards App</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+          <Container>
+              <Link class="nav-link" to={"/collections"}>Collections</Link>
+              { user ? (
+              <Link class="nav-link" >Logout ({user})</Link>
+              ) : (
+              <>
+                <Link class="nav-link" to={"/login"}>Login</Link>
+                <Link class="nav-link" to={"/signup"}>Sign Up</Link>
+              </>
+              )}
+            </Container>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+      </div>
+    </Navbar>
+<br/>
+
       <Login />
 
     </div>
